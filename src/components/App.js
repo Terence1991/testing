ca5b4aa6b3bd848ec80fca6440967615de04b067
terence1991 <cplterence@gmail.com>
2019-04-04 00:03:18 -0400

    basic components added for testing

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CommentBox from './CommentBox.js'
+import CommentList from './CommentList.js'
+
+
+export default () => {
+  return (
+    <div>
+     <CommentBox/>
+     <CommentList/>
+    </div>
+  )
+}
