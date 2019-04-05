b492ff7ac4baa648fd2d77c6532e0d4d30bf73e3
terence1991 <cplterence@gmail.com>
2019-04-05 09:59:09 -0400

    testing Comment box in app

--- src/components/_Tests_/App.test.js
+++ src/components/_Tests_/App.test.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import App from '../App'
+import { shallow } from 'enzyme'
+import CommentBox from '../CommentBox'
 
 
 it('shows comment box', () => {
-  const div = document.createElement("div")
-  ReactDOM.render(<App />, div )
+  const component = shallow(<App />)
 
-
- expect(div.innerHTML).toContain('comment box')
-
-  ReactDOM.unmountComponentAtNode(div)
+  expect(component.find(CommentBox).length).toEqual(1)
 })
