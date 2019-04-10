9d456b273466dd10ec0eac42994b5a9fe389737d
terence1991 <cplterence@gmail.com>
2019-04-10 09:26:02 -0400

    text area test

--- src/Components/_Tests_/CommentBox.test.js
+++ src/Components/_Tests_/CommentBox.test.js
@@ -17,3 +17,13 @@ it('has a  textarea and button', () => {
   expect(component.find('textarea').length)
   expect(component.find('button').length)
 })
+
+//forced to rerender component because setState is aysync
+it('has a text area', () => {
+  component.find('textarea').simulate('change', {
+    target: {
+      value: 'new comment'
+    }
+  })
+  component.update()
+})
