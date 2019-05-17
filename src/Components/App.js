88248f0684aef091ff5808d30d1a57ef6fc041f9
terence1991 <cplterencehawes@gmail.com>
2019-05-17 14:25:27 -0400

    render header

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,9 +2,29 @@ import React, {Component} from 'react'
 import CommentBox from 'Components/CommentBox'
 import CommentList from 'Components/CommentList.js' 
 import { Route, Link } from 'react-router-dom'
+import connect from 'react-rotuer'
+import auth from '../reducers/auth';
 
 
 class App extends Component {
+
+  renderButton() {
+    if(this.props.auth) {
+     return(
+     <button>
+        sign out 
+      </button>
+     )
+    }  else {
+      return (
+        <button>
+          sign in
+        </button>
+      )
+    }
+  }
+
+
   renderHeader() {
    return(
    <ul>
@@ -24,9 +44,18 @@ class App extends Component {
   render() {
     return (
       <div>
+        {this.renderHeader()}
       <Route path="/post" component={CommentBox} />
       <Route path="/" exact component={CommentList} />
     </div>
   )
   }
 }
+
+function mapStateToProps(state) {
+  return { 
+    auth: state.auth
+  }
+}
+
+export default connect(mapStateToProps)(App)
